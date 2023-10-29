cea40a5a019c52d5173eda89d40c99cb671e8c34
Vimalsharma8085 <142784369+Vimalsharma8085@users.noreply.github.com>
2023-10-29 11:02:23 +0530

    changes done

---
+++ src/main/java/com/Mct/food/delivery/platform/Model/Enums/Specialty.java
@@ -0,0 +1,13 @@
+package com.Mct.food.delivery.platform.Model.Enums;
+
+public enum Specialty {
+
+    Eggs_Benedict,
+    Gourmet_Pizza,
+
+    Paella,
+    Sushi_Rolls,
+    Tandoori_Chicken
+
+
+}
